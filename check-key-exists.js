7e0b248ce6960680dd23101428f87385ce4bf810
Kushal <kushaldeveloper@gmail.com>
2024-07-12 16:04:00 -0400

    update test
    PS C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists> npx playwright test check-key-exists.test.js
    
    Running 3 tests using 1 worker
    
         1 check-key-exists.test.js:66:5 › Performance Tests › == undefined
    
      ✘  1 check-key-exists.test.js:66:5 › Performance Tests › == undefined (81ms)
      ✘  2 check-key-exists.test.js:73:5 › Performance Tests › "key" in testObj (77ms)
      ✘  3 check-key-exists.test.js:80:5 › Performance Tests › hasOwnProperty (81ms)
    
      1) check-key-exists.test.js:66:5 › Performance Tests › == undefined ──────────────────────────────
    
        Error: page.evaluate: ReferenceError: testPerformance is not defined
    
            at eval (eval at evaluate (:226:30), <anonymous>:2:28)
            at UtilityScript.evaluate (<anonymous>:228:17)
            at UtilityScript.<anonymous> (<anonymous>:1:44)
            at eval (C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists\eval at evaluate (:226:30), <anonymous>:2:28)
            at UtilityScript.evaluate (C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists\<anonymous>:228:17)
            at UtilityScript.<anonymous> (C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists\<anonymous>:1:44)
            at C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists\check-key-exists.test.js:67:20
    
      2) check-key-exists.test.js:73:5 › Performance Tests › "key" in testObj ──────────────────────────
    
        Error: page.evaluate: ReferenceError: testPerformance is not defined
    
            at eval (eval at evaluate (:226:30), <anonymous>:2:28)
            at UtilityScript.evaluate (<anonymous>:228:17)
            at UtilityScript.<anonymous> (<anonymous>:1:44)
            at eval (C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists\eval at evaluate (:226:30), <anonymous>:2:28)
            at UtilityScript.evaluate (C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists\<anonymous>:228:17)
            at UtilityScript.<anonymous> (C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists\<anonymous>:1:44)
            at C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists\check-key-exists.test.js:74:20
    
      3) check-key-exists.test.js:80:5 › Performance Tests › hasOwnProperty ────────────────────────────
    
        Error: page.evaluate: ReferenceError: testPerformance is not defined
    
            at eval (eval at evaluate (:226:30), <anonymous>:2:28)
            at UtilityScript.evaluate (<anonymous>:228:17)
            at UtilityScript.<anonymous> (<anonymous>:1:44)
            at eval (C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists\eval at evaluate (:226:30), <anonymous>:2:28)
            at UtilityScript.evaluate (C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists\<anonymous>:228:17)
            at UtilityScript.<anonymous> (C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists\<anonymous>:1:44)
            at C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists\check-key-exists.test.js:81:20
    
      3 failed
        check-key-exists.test.js:66:5 › Performance Tests › == undefined ───────────────────────────────
        check-key-exists.test.js:73:5 › Performance Tests › "key" in testObj ───────────────────────────
        check-key-exists.test.js:80:5 › Performance Tests › hasOwnProperty ─────────────────────────────
    PS C:\Users\kushal\src\myhtml\javascript-benchmarks-check-key-exists>

--- check-key-exists.js
+++ check-key-exists.js
@@ -1,86 +1,74 @@
-const { test, expect } = require('@playwright/test');
+const playwright = require('playwright');
 
-const testObj = {
-    a: 28, b: 82, c: "hello", d: 983, e: 'lara', o: "key", f: '82828', g: 8,
-    h: 45, i: "world", j: 123, k: 'test', l: 999, m: 'data', n: 456, p: 'value',
-    q: 789, r: 'example', s: 321, t: 'sample', u: 654, v: 'info', w: 987, x: 'text',
-    y: 111, z: 'string', aa: 222, bb: 'char', cc: 333, dd: 'num', ee: 444, ff: 'bool',
-    gg: 555, hh: 'array', ii: 666, jj: 'object', kk: 777, ll: 'function', mm: 888, nn: 'method',
-    oo: 999, pp: 'property', qq: 101, rr: 'key', ss: 202, tt: 'value', uu: 303, vv: 'entry',
-    ww: 404, xx: 'element', yy: 505, zz: 'node', aaa: 606, bbb: 'item', ccc: 707, ddd: 'part',
-    eee: 808, fff: 'piece', ggg: 909, hhh: 'segment', iii: 1010, jjj: 'section', kkk: 1111, lll: 'portion',
-    mmm: 1212, nnn: 'fraction', ooo: 1313, ppp: 'bit', qqq: 1414, rrr: 'chunk', sss: 1515, ttt: 'slice',
-    uuu: 1616, vvv: 'division', www: 1717, xxx: 'share', yyy: 1818, zzz: 'part', aaaa: 1919, bbbb: 'piece',
-    cccc: 2020, dddd: 'segment', eeee: 2121, ffff: 'section', gggg: 2222, hhhh: 'portion', iiii: 2323, jjjj: 'fraction',
-    kkkk: 2424, llll: 'bit', mmmm: 2525, nnnn: 'chunk', oooo: 2626, pppp: 'slice', qqqq: 2727, rrrr: 'division',
-    ssss: 2828, tttt: 'share', uuuu: 2929, vvvv: 'part', wwww: 3030, xxxx: 'piece', yyyy: 3131, zzzz: 'segment'
-};
+(async () => {
+  const browser = await playwright.chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-test.describe('Performance Tests', () => {
-    test('== undefined', async ({ page }) => {
-        await page.evaluate(() => {
-            const testObj = {
-                a: 28, b: 82, c: "hello", d: 983, e: 'lara', o: "key", f: '82828', g: 8,
-                h: 45, i: "world", j: 123, k: 'test', l: 999, m: 'data', n: 456, p: 'value',
-                q: 789, r: 'example', s: 321, t: 'sample', u: 654, v: 'info', w: 987, x: 'text',
-                y: 111, z: 'string', aa: 222, bb: 'char', cc: 333, dd: 'num', ee: 444, ff: 'bool',
-                gg: 555, hh: 'array', ii: 666, jj: 'object', kk: 777, ll: 'function', mm: 888, nn: 'method',
-                oo: 999, pp: 'property', qq: 101, rr: 'key', ss: 202, tt: 'value', uu: 303, vv: 'entry',
-                ww: 404, xx: 'element', yy: 505, zz: 'node', aaa: 606, bbb: 'item', ccc: 707, ddd: 'part',
-                eee: 808, fff: 'piece', ggg: 909, hhh: 'segment', iii: 1010, jjj: 'section', kkk: 1111, lll: 'portion',
-                mmm: 1212, nnn: 'fraction', ooo: 1313, ppp: 'bit', qqq: 1414, rrr: 'chunk', sss: 1515, ttt: 'slice',
-                uuu: 1616, vvv: 'division', www: 1717, xxx: 'share', yyy: 1818, zzz: 'part', aaaa: 1919, bbbb: 'piece',
-                cccc: 2020, dddd: 'segment', eeee: 2121, ffff: 'section', gggg: 2222, hhhh: 'portion', iiii: 2323, jjjj: 'fraction',
-                kkkk: 2424, llll: 'bit', mmmm: 2525, nnnn: 'chunk', oooo: 2626, pppp: 'slice', qqqq: 2727, rrrr: 'division',
-                ssss: 2828, tttt: 'share', uuuu: 2929, vvvv: 'part', wwww: 3030, xxxx: 'piece', yyyy: 3131, zzzz: 'segment'
-            };
+  await page.goto('about:blank');
 
-            function testPerformance(fn, iterations = 10000) {
-                const times = [];
-                // Warm-up iterations
-                for (let i = 0; i < 1000; i++) {
-                    fn();
-                }
-                // Actual measurement
-                for (let i = 0; i < iterations; i++) {
-                    const start = performance.now();
-                    fn();
-                    const end = performance.now();
-                    const duration = end - start;
-                    if (duration > 0) {
-                        times.push(duration);
-                    }
-                }
-                times.sort((a, b) => a - b);
-                const min = times[0];
-                const max = times[times.length - 1];
-                const median = times[Math.floor(times.length / 2)];
-                const ninetiethPercentile = times[Math.floor(times.length * 0.9)];
-                const ninetyNinthPercentile = times[Math.floor(times.length * 0.99)];
-                return { min, max, median, ninetiethPercentile, ninetyNinthPercentile };
-            }
+  await page.evaluate(() => {
+    const testObj = {
+      a: 28, b: 82, c: "hello", d: 983, e: 'lara', o: "key", f: '82828', g: 8,
+      h: 45, i: "world", j: 123, k: 'test', l: 999, m: 'data', n: 456, p: 'value',
+      q: 789, r: 'example', s: 321, t: 'sample', u: 654, v: 'info', w: 987, x: 'text',
+      y: 111, z: 'string', aa: 222, bb: 'char', cc: 333, dd: 'num', ee: 444, ff: 'bool',
+      gg: 555, hh: 'array', ii: 666, jj: 'object', kk: 777, ll: 'function', mm: 888, nn: 'method',
+      oo: 999, pp: 'property', qq: 101, rr: 'key', ss: 202, tt: 'value', uu: 303, vv: 'entry',
+      ww: 404, xx: 'element', yy: 505, zz: 'node', aaa: 606, bbb: 'item', ccc: 707, ddd: 'part',
+      eee: 808, fff: 'piece', ggg: 909, hhh: 'segment', iii: 1010, jjj: 'section', kkk: 1111, lll: 'portion',
+      mmm: 1212, nnn: 'fraction', ooo: 1313, ppp: 'bit', qqq: 1414, rrr: 'chunk', sss: 1515, ttt: 'slice',
+      uuu: 1616, vvv: 'division', www: 1717, xxx: 'share', yyy: 1818, zzz: 'part', aaaa: 1919, bbbb: 'piece',
+      cccc: 2020, dddd: 'segment', eeee: 2121, ffff: 'section', gggg: 2222, hhhh: 'portion', iiii: 2323, jjjj: 'fraction',
+      kkkk: 2424, llll: 'bit', mmmm: 2525, nnnn: 'chunk', oooo: 2626, pppp: 2627, qqqq: 2727, rrrr: 'division',
+      ssss: 2828, tttt: 2929, uuuu: 2930, vvvv: 'part', wwww: 3030, xxxx: 'piece', yyyy: 3131, zzzz: 'segment'
+    };
 
-            const test1 = () => {
-                let result;
-                if (testObj.key !== undefined) {
-                    result = true;
-                }
-            };
+    function test1() {
+      let result;
+      if (testObj.key !== undefined) {
+        result = true;
+      }
+      return result;
+    }
 
-            const test1Results = testPerformance(test1);
-            console.log(`== undefined - Min: ${test1Results.min} ms, Max: ${test1Results.max} ms, Median: ${test1Results.median} ms, 90th Percentile: ${test1Results.ninetiethPercentile} ms, 99th Percentile: ${test1Results.ninetyNinthPercentile} ms`);
-        });
-    });
+    function test2() {
+      let result;
+      if ("key" in testObj) {
+        result = true;
+      }
+      return result;
+    }
 
-    test('"key" in testObj', async ({ page }) => {
-        await page.evaluate(() => {
-            const testObj = {
-                a: 28, b: 82, c: "hello", d: 983, e: 'lara', o: "key", f: '82828', g: 8,
-                h: 45, i: "world", j: 123, k: 'test', l: 999, m: 'data', n: 456, p: 'value',
-                q: 789, r: 'example', s: 321, t: 'sample', u: 654, v: 'info', w: 987, x: 'text',
-                y: 111, z: 'string', aa: 222, bb: 'char', cc: 333, dd: 'num', ee: 444, ff: 'bool',
-                gg: 555, hh: 'array', ii: 666, jj: 'object', kk: 777, ll: 'function', mm: 888, nn: 'method',
-                oo: 999, pp: 'property', qq: 101, rr: 'key', ss: 202, tt: 'value', uu: 303, vv: 'entry',
-                ww: 404, xx: 'element', yy: 505, zz: 'node', aaa: 606, bbb: 'item', ccc: 707, ddd: 'part',
-                eee: 808, fff: 'piece', ggg: 909, hhh: 'segment', iii: 1010, jjj: 'section', kkk: 1111, lll: 'portion',
-                mmm: 121
+    function test3() {
+      let result;
+      if (testObj.hasOwnProperty("key")) {
+        result = true;
+      }
+      return result;
+    }
+
+    console.log("Test 1:");
+    console.time("test1");
+    for (let i = 0; i < 100000; i++) {
+      test1();
+    }
+    console.timeEnd("test1");
+
+    console.log("Test 2:");
+    console.time("test2");
+    for (let i = 0; i < 100000; i++) {
+      test2();
+    }
+    console.timeEnd("test2");
+
+    console.log("Test 3:");
+    console.time("test3");
+    for (let i = 0; i < 100000; i++) {
+      test3();
+    }
+    console.timeEnd("test3");
+  });
+
+  await browser.close();
+})();
